c7437b92a072e1ac0ace42d3804a889cdb9622a2
XinyuanYxy <yxychris@outlook.com>
2023-04-26 22:06:14 -0400

    add up,down, left, right 4 directions

--- src/com/yxy/tankGame/MyPanel.java
+++ src/com/yxy/tankGame/MyPanel.java
@@ -21,7 +21,12 @@ public class MyPanel extends JPanel {
         g.fillRect(0,0, 1000, 1000);
         // draw myTank
         drawTank(myTank.getX(), myTank.getY(), g, 0, 0, 1);
-        drawTank(100, 100, g, 0, 1, 2);
+        // face right
+        drawTank(100, 100, g, 3, 1, 2);
+        // face down
+        drawTank(200, 200, g, 1, 1, 2);
+        // face left
+        drawTank(300, 300, g, 2, 1, 2);
 
 
     }
@@ -37,16 +42,16 @@ public class MyPanel extends JPanel {
      */
     public void drawTank(int x, int y, Graphics g, int direction, int type, int scale){
         switch (type){
-            case 0: // 自己的tank
+            case 0: // player's tank
                 g.setColor(Color.CYAN);
                 break;
-            case 1: // 敌人tank
+            case 1: // robotic tank
                 g.setColor(Color.yellow);
                 break;
         }
-
+        // 0 up, 1 down, 2 left, 3 right
         switch (direction){
-            case 0: // face upward
+            case 0:
                 // wheels: width 6, height 32
                 // left wheel
                 g.fill3DRect(x, y, 6 * scale, 32 * scale, false);
@@ -57,7 +62,44 @@ public class MyPanel extends JPanel {
                 // right wheel
                 g.fill3DRect(x + 18 * scale,y,6 * scale ,32 * scale ,false);
                 // pipeline
-                g.drawLine(x + 12 * scale,y - 20,x + 12 * scale, y + 26);
+                g.drawLine(x + 12 * scale,y - 20 * scale,x + 12 * scale, y + 26 * scale);
+                break;
+            case 1:
+                // left wheel
+                g.fill3DRect(x, y, 6 * scale, 32 * scale, false);
+                // body: width: 12, height: 22
+                g.fill3DRect(x + 6 * scale, y + 5 * scale, 12 * scale, 22 * scale, false);
+                // draw left corner of the oval
+                g.fillOval(x + 6 * scale,y + 12 * scale,12 * scale,12 * scale);
+                // right wheel
+                g.fill3DRect(x + 18 * scale,y,6 * scale ,32 * scale ,false);
+                // pipeline
+                g.drawLine(x + 12 * scale,y + 47 * scale,x + 12 * scale, y + 27 * scale);
+                break;
+            case 2: // left
+                // left wheel
+                g.fill3DRect(x, y,  32 * scale, 6 * scale, false);
+                // body: width: 12, height: 22
+                g.fill3DRect(x + 5 * scale, y + 6 * scale, 22 * scale, 12 * scale, false);
+                // draw left corner of the oval
+                g.fillOval(y + 8 * scale, x + 6 * scale,12 * scale,12 * scale);
+                // right wheel
+                g.fill3DRect(x ,y + 18 * scale,32 * scale, 6 * scale  ,false);
+                // pipeline
+                g.drawLine(x - 10 * scale,y + 12 * scale,x + 18 * scale, y + 12 * scale);
+                break;
+            case 3: // right
+                // left wheel
+                g.fill3DRect(x, y,  32 * scale, 6 * scale, false);
+                // body: width: 12, height: 22
+                g.fill3DRect(x + 5 * scale, y + 6 * scale, 22 * scale, 12 * scale, false);
+                // draw left corner of the oval
+                g.fillOval(y + 12 * scale, x + 6 * scale,12 * scale,12 * scale);
+                // right wheel
+                g.fill3DRect(x ,y + 18 * scale,32 * scale, 6 * scale  ,false);
+                // pipeline
+                g.drawLine(x + 42 * scale,y + 12 * scale,x + 18 * scale, y + 12 * scale);
+                break;
         }
 
 
---
+++ src/readme.md
@@ -0,0 +1,48 @@
+# TankShooting Game
+
+## Description
+
+Build a tank shooting game in order to know java thread
+better.
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+## Features
+
+If your project has a lot of features, list them here.
+
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
